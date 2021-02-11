abd96851ce9e3e07da1182e8ddec02fc16b10e34
Use your GitHub usernameedenborrowman <bor20025@byui.edu>
2021-02-10 19:49:03 -0500

    Math.round adjustment for WC

--- assignments/lesson6/js/windchill.js
+++ assignments/lesson6/js/windchill.js
@@ -1,8 +1,6 @@
 let t = parseFloat(document.getElementById("temp").innerHTML);
 let w = parseFloat(document.getElementById("speed").innerHTML);
 
-console.log(t);
-console.log(w);
 let wc =
   35.74 +
   0.6215 * t -
@@ -10,4 +8,4 @@ let wc =
   0.4275 * t * Math.pow(w, 0.16);
 
 document.getElementById("windchill_num").innerHTML =
-  wc.toFixed(2) + "&deg; " + "F";
+  Math.round(wc) + "&deg; " + "F";
