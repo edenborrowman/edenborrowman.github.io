8605a4771ad0114635ab536c25142fd4c4a40726
Use your GitHub usernameedenborrowman <bor20025@byui.edu>
2021-02-11 21:37:22 -0500

    javascript updates for windchill calculaton using parameters

--- assignments/lesson6/js/windchill.js
+++ assignments/lesson6/js/windchill.js
@@ -1,11 +1,17 @@
 let t = parseFloat(document.getElementById("temp").innerHTML);
 let w = parseFloat(document.getElementById("speed").innerHTML);
 
-let wc =
-  35.74 +
-  0.6215 * t -
-  35.75 * Math.pow(w, 0.16) +
-  0.4275 * t * Math.pow(w, 0.16);
+if (t <= 50 && w >= 3) {
+  let wc =
+    35.74 +
+    0.6215 * t -
+    35.75 * Math.pow(w, 0.16) +
+    0.4275 * t * Math.pow(w, 0.16);
 
-document.getElementById("windchill_num").innerHTML =
-  Math.round(wc) + "&deg; " + "F";
+  wc = document.getElementById("windchill_num").innerHTML =
+    Math.round(wc) + "&deg; " + "F";
+} else {
+  wc = "Not Applicable";
+
+  document.getElementById("windchill_num").innerHTML = wc;
+}
