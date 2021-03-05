8cc304aa5cc311d6897614834c99b7dbc039ca54
Use your GitHub usernameedenborrowman <bor20025@byui.edu>
2021-03-04 21:16:04 -0500

    prophets cards

---
+++ assignments/lesson-9/js/prophets.js
@@ -0,0 +1,36 @@
+const requestURL = "https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);
+    // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    // loop through array
+    for (let i = 0; i < prophets.length; i++) {
+    // declare each variable for what the card will need
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let line1 = document.createElement('p');
+      let line2 = document.createElement('p');
+      let image = document.createElement('img');
+      
+      // Sets out what each card will show and concatenates the strings
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      line1.textContent = 'Born:' + ' ' + prophets[i].birthdate;  
+      line2.textContent = 'Place:' + ' ' + prophets[i].birthplace;
+      image.setAttribute('src', prophets[i].imageurl,);
+      image.setAttribute('alt', prophets[i].name + " " + prophets[i].lastname + " " + "-" + " " + prophets[i].order);
+      
+      // build the display by updating for each item in the loop
+         card.appendChild(h2);
+         card.appendChild(line1);
+         card.appendChild(line2);
+         card.appendChild(image);
+
+      document.querySelector('div.cards').appendChild(card);
+    }
+
+    });
