7e8168b8f10fdd61a6e2c5879c10bf4f88a0c5c8
Use your GitHub usernameedenborrowman <bor20025@byui.edu>
2021-03-12 18:46:31 -0500

    css tweaks for 5 day

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -51,6 +51,7 @@ const apiforecastURL =
 fetch(apiforecastURL)
   .then((response) => response.json())
   .then((jsObject) => {
+
     // Create an array with day names to use for five day forecast
     const dayName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -67,25 +68,18 @@ fetch(apiforecastURL)
       let d = new Date(thefive[i].dt_txt); //date object to get date
 
       //write day name using array built earlier
-      document.getElementById("dayofweek" + (day + 1)).textContent =
-        dayName[d.getDay()];
+      document.getElementById("dayofweek" + (day + 1)).textContent = dayName[d.getDay()];
 
       // write temperature data. Use Math.round to make it a whole number.
-      document.getElementById("forecast" + (day + 1)).textContent = Math.round(
-        thefive[day].main.temp
-      );
+      document.getElementById("forecast" + (day + 1)).textContent = Math.round(thefive[day].main.temp);
 
       // give the browser the icon address
-      var imagesrc =
-        "https://openweathermap.org/img/w/" +
-        thefive[day].weather[0].icon +
-        ".png";
+      var imagesrc = "https://openweathermap.org/img/w/" + thefive[day].weather[0].icon + ".png";
 
       // populate table with icons, set alt attribute using weather description from the object
       document.getElementById("imagesrc" + (day + 1)).textContent = imagesrc;
       document.getElementById("icon" + (day + 1)).setAttribute("src", imagesrc);
-      document.getElementById("icon" + (day + 1));
-      document.setAttribute("alt", thefive[0].weather[0].description);
+      document.getElementById("icon" + (day + 1)).setAttribute("alt", thefive[0].weather[0].description);
 
       day++;
     }
