0fb41bc52e7c604e6fbec08b5a7f8c5c7e08ead0
Use your GitHub usernameedenborrowman <bor20025@byui.edu>
2021-03-12 02:57:01 -0500

    weather forecast

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -11,13 +11,13 @@ fetch(apiURL)
     
  // assign js Objects to elements by ID. Pay close attention to which data is in an array.
     document.getElementById('cond').textContent = jsObject.weather[0].main;
-    document.getElementById('temp').textContent = jsObject.main.temp;
-    document.getElementById('speed').textContent = jsObject.wind.speed;
+    document.getElementById('temp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('speed').textContent = Math.round(jsObject.wind.speed);
     document.getElementById('humid_pct').textContent = jsObject.main.humidity + "%";
 
 // Use weather API data to calculate wind chill. Be sure to parseFloat to convert string data to numbers.
-    let t = parseFloat(document.getElementById("temp").innerHTML);
-    let w = parseFloat(document.getElementById("speed").innerHTML);
+    let t = jsObject.main.temp;
+    let w = jsObject.wind.speed;
      
     if (t <= 50 && w >= 3) {
       let wc =
@@ -37,102 +37,44 @@ fetch(apiURL)
 
 
 // List the URL of the API forecast data
-const apiforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=31faaedc2de9ab98c63a927fb8525e85"
+const apiforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=31faaedc2de9ab98c63a927fb8525e85";
 
 // As before, fetch the forecast data, create the JSON object, console log to make sure it worked.
 
 fetch(apiforecastURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject); 
-
     
-// Use API data to place names of next 5 days in table using a loop
-   
-    // step 1. Create new date object
-    // step 2. Use .tolocaleString() to extract day name
-    // step 3. Create subsequent 4 days (how do I do this with a loop???)
-    
-    
-    const dateObject = new Date()
-    const d1 = dateObject.toLocaleString('en-US', {weekday: "long"});
-    console.log(d1);
-        
-    const plus1 = new Date(dateObject)
-    plus1.setDate(plus1.getDate() + 1)
-    d2 = plus1.toLocaleString('en-US', {weekday: "long"});
-    console.log(d2);
-
-    const plus2 = new Date(dateObject)
-    plus2.setDate(plus2.getDate() + 2)
-    d3 = plus2.toLocaleString('en-US', {weekday: "long"});
-    console.log(d3);
-
-    const plus3 = new Date(dateObject)
-    plus3.setDate(plus3.getDate() + 3)
-    d4 = plus3.toLocaleString('en-US', {weekday: "long"});
-    console.log(d4);
-
-    const plus4 = new Date(dateObject)
-    plus4.setDate(plus4.getDate() + 4)
-    d5 = plus4.toLocaleString('en-US', {weekday: "long"});
-    console.log(d5);
+// Create an array with day names to use for five day forecast
  
-    // Populate the table with responsive data
+ const dayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-      document.getElementById("d1").textContent = d1;  
-      document.getElementById("d2").textContent = d2;
-      document.getElementById("d3").textContent = d3;
-      document.getElementById("d4").textContent = d4;
-      document.getElementById("d5").textContent = d5;
+ // Filter the jsObject to only show data from 6:00 pm (18:00:00)
+ const thefive = jsObject.list.filter((element) => element.dt_txt.includes('18:00:00'));
+ 
+
+ // Use a for loop to loop through the data and fill the table
+ 
+ let day = 0;
+ let i= 0;
+ 
+ for (i=0; i<thefive.length; i++) {
+      
+  let d = new Date(thefive[i].dt_txt);  //date object to get date
    
-
-// Populate the temperature values in the 5 day by ID ??how to sort by date???
-
-  document.getElementById('t1').textContent = jsObject.list[0].main.temp + "\°";
-  document.getElementById('t2').textContent = jsObject.list[1].main.temp + "\°";
-  document.getElementById('t3').textContent = jsObject.list[2].main.temp + "\°";
-  document.getElementById('t4').textContent = jsObject.list[3].main.temp + "\°";
-  document.getElementById('t5').textContent = jsObject.list[4].main.temp + "\°";
-
-
-// Use API to change weather icons. Note that there are _two_ arrays to account for here!
-
-    const imagesrc1 = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';
-    const imagesrc2 = 'https://openweathermap.org/img/w/' + jsObject.list[1].weather[0].icon + '.png';
-    const imagesrc3 = 'https://openweathermap.org/img/w/' + jsObject.list[2].weather[0].icon + '.png';
-    const imagesrc4 = 'https://openweathermap.org/img/w/' + jsObject.list[3].weather[0].icon + '.png';
-    const imagesrc5 = 'https://openweathermap.org/img/w/' + jsObject.list[4].weather[0].icon + '.png';
-          
-    document.getElementById('imagesrc1').textContent = imagesrc1;
-    document.getElementById('icon1').setAttribute('src', imagesrc1);
-    document.getElementById('icon1').setAttribute('alt', jsObject.list[0].weather[0].description);
-
-    document.getElementById('imagesrc2').textContent = imagesrc2;
-    document.getElementById('icon2').setAttribute('src', imagesrc2);
-    document.getElementById('icon2').setAttribute('alt', jsObject.list[1].weather[0].description);
-
-    document.getElementById('imagesrc3').textContent = imagesrc3;
-    document.getElementById('icon3').setAttribute('src', imagesrc3);
-    document.getElementById('icon3').setAttribute('alt', jsObject.list[2].weather[0].description);
-
-    document.getElementById('imagesrc4').textContent = imagesrc4;
-    document.getElementById('icon4').setAttribute('src', imagesrc4);
-    document.getElementById('icon4').setAttribute('alt', jsObject.list[3].weather[0].description);
-
-    document.getElementById('imagesrc5').textContent = imagesrc5;
-    document.getElementById('icon5').setAttribute('src', imagesrc5);
-    document.getElementById('icon5').setAttribute('alt', jsObject.list[4].weather[0].description);
-
-
-
-// const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-// const desc = jsObject.weather[0].description;  // note how we reference the weather array
-// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-// document.getElementById('icon').setAttribute('alt', desc);
-
-
-
-});
+  //write day name using array built earlier
+  document.getElementById('dayofweek' + (day+1)).textContent = dayName[d.getDay()];  
+  // write temperature data. Use Math.round to make it a whole number.
+  document.getElementById('forecast' + (day+1)).textContent = Math.round(jsObject.list[i].main.temp);
+  // give the browser the icon address
+  var imagesrc = 'https://openweathermap.org/img/w/' + thefive[0].weather[0].icon + '.png';
+  // populate table with icons, set alt attribute using weather description from the object
+  document.getElementById('imagesrc' + (day+1)).textContent = imagesrc;
+  document.getElementById('icon' + (day+1)).setAttribute('src', imagesrc);
+  document.getElementById('icon' + (day+1)).setAttribute('alt', thefive[0].weather[0].description);
+    
+    day++;
+} 
+ 
+});  
 
