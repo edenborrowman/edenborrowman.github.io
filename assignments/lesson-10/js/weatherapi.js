955cae24f2869eb4d9d945b592cc671b2a17f52c
Use your GitHub usernameedenborrowman <bor20025@byui.edu>
2021-03-12 18:29:41 -0500

    semicolon

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -84,7 +84,7 @@ fetch(apiforecastURL)
       // populate table with icons, set alt attribute using weather description from the object
       document.getElementById("imagesrc" + (day + 1)).textContent = imagesrc;
       document.getElementById("icon" + (day + 1)).setAttribute("src", imagesrc);
-      document.getElementById("icon" + (day + 1))
+      document.getElementById("icon" + (day + 1));
       document.setAttribute("alt", thefive[0].weather[0].description);
 
       day++;
